5fcb6f5aecb2c5eae3ead14306fcabd743932c6a
quek0504 <quek0504@gmail.com>
2020-12-03 20:36:17 +0800

    added buttons in tree nodes

--- src/pages/product/category/index.jsx
+++ src/pages/product/category/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
-import { Card, Tree } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Space, Divider, Card, Tree, Typography } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import { connect } from 'umi';
 import styles from './style.less';
 
 const { TreeNode } = Tree;
+
+const { Text } = Typography;
 
 export const ProductCategory = (props) => {
   const {
@@ -12,17 +14,106 @@ export const ProductCategory = (props) => {
     productCategory, // data from model
   } = props;
 
+  // Tree related state
+  const [treeExpandedKeys, setTreeExpandedKeys] = useState([]);
+  const [treeCheckedKeys, setTreeCheckedKeys] = useState([]);
+  const [treeSelectedKeys, setTreeSelectedKeys] = useState([]);
+  const [autoExpandParent, setAutoExpandParent] = useState(true);
+
+  // Modal related state
+  // const [done, setDone] = useState(false);
+  // const [visible, setVisible] = useState(false);
+  // const [current, setCurrent] = useState(undefined);
+
   useEffect(() => {
     dispatch({
       type: 'productCategory/fetch',
     });
   }, [1]);
 
+  // Tree related function
+  const onExpand = (expandedKeys) => {
+    console.log('onExpand', expandedKeys);
+    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+    setTreeExpandedKeys(expandedKeys);
+    setAutoExpandParent(false);
+  };
+
+  const onCheck = (checkedKeys) => {
+    console.log('onCheck', checkedKeys);
+    setTreeCheckedKeys(checkedKeys);
+  };
+
+  const onSelect = (selectedKeys, info) => {
+    console.log('onSelect', info);
+    setTreeSelectedKeys(selectedKeys);
+  };
+
+  // Modal related function
+  const showAppendModal = (item) => {
+    console.log(item);
+    // setVisible(true);
+    // setCurrent(item);
+  };
+
+  const showEditModal = (item) => {
+    console.log(item);
+    // setVisible(true);
+    // setCurrent(item);
+  };
+
+  const showDeleteModal = (item) => {
+    console.log(item);
+    // setVisible(true);
+    // setCurrent(item);
+  };
+
+  // render content
+  const treeNodeContent = (item) => {
+    return (
+      <Space size="large" split={<Divider type="vertical" />}>
+        <Text strong>{item.name}</Text>
+        {item.catLevel <= 2 ? (
+          <a
+            key="append"
+            onClick={(e) => {
+              e.preventDefault();
+              showAppendModal(item);
+            }}
+          >
+            Append
+          </a>
+        ) : null}
+        <a
+          key="edit"
+          onClick={(e) => {
+            e.preventDefault();
+            showEditModal(item);
+          }}
+        >
+          Edit
+        </a>
+        {item.children.length === 0 ? (
+          <a
+            key="delete"
+            onClick={(e) => {
+              e.preventDefault();
+              showDeleteModal(item);
+            }}
+          >
+            Delete
+          </a>
+        ) : null}
+      </Space>
+    );
+  };
+
   const renderTreeNodes = (data) =>
     data.map((item) => {
       if (item.children) {
         return (
-          <TreeNode title={item.name} key={item.catId}>
+          <TreeNode title={treeNodeContent(item)} key={item.catId}>
             {renderTreeNodes(item.children)}
           </TreeNode>
         );
@@ -45,7 +136,18 @@ export const ProductCategory = (props) => {
             }}
           >
             {/* Tree inside card */}
-            <Tree>{renderTreeNodes(productCategory.data)}</Tree>
+            <Tree
+              checkable
+              onExpand={onExpand}
+              expandedKeys={treeExpandedKeys}
+              autoExpandParent={autoExpandParent}
+              onCheck={onCheck}
+              checkedKeys={treeCheckedKeys}
+              onSelect={onSelect}
+              selectedKeys={treeSelectedKeys}
+            >
+              {renderTreeNodes(productCategory.data)}
+            </Tree>
           </Card>
         </div>
       </PageContainer>
